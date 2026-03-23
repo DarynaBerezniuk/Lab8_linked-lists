e918d7e949425f6e2c2dacaefe6b3f0a591431a1
DarynaBerezniuk <berezniukdaryna@gmail.com>
2026-03-23 13:58:03 +0200

    finished move node task

---
+++ move_node/main.py
@@ -0,0 +1,15 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+    
+class Context(object):
+    def __init__(self, source, dest):
+        self.source = source
+        self.dest = dest
+    
+def move_node(source, dest):
+    data = Node(source.data)
+    source = source.next
+    data.next = dest
+    return Context(source, data)
